000418f3c1b05157438c869d3105ba97c6e7c306
PremJP777 <premsagar.jonnalagadda@gmail.com>
2024-07-17 18:33:31 +0530

    Task-2

--- src/CreateSlice.jsx
+++ src/CreateSlice.jsx
@@ -1,23 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CreatSlice = createSlice({
+export const CreateSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
+      const {name,image,cost}=action.payload;
+      const existing=state.items.find((plant)=>plant.name===name);
+      if(existing){
+        existing.quantity++;
+      }else{
+        state.items.push({name,image,cost,quantity:1});
+      }
     
     },
     removeItem: (state, action) => {
+      state.items=state.items.filter((plant)=>plant.name!==action.payload); 
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name,amount}=action.payload;
+      const existing=state.items.find((plant)=>plant.name===name);
+      if(existing){
+        existing.quantity=amount;
+      } 
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
+export const { addItem, removeItem, updateQuantity } = CreateSlice.actions;
 
-export default CreatSlice.reducer;
+export default CreateSlice.reducer;
